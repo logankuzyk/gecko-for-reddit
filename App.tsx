f163978b7469bd8f614034a093b90c90330d718b
Logan Kuzyk <logan@kuzyk.net>
2021-12-05 13:08:52 -0800

    Add react-query

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const queryClient = new QueryClient();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <QueryClientProvider client={queryClient}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </QueryClientProvider>
       </SafeAreaProvider>
     );
   }
