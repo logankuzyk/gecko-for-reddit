4b563f2b1c650287856f78569c00ab737c5717fd
Logan Kuzyk <logan@kuzyk.net>
2021-12-20 15:59:56 -0800

    Initial auth implementation

--- contexts/RedditContext.tsx
+++ contexts/RedditContext.tsx
@@ -1,12 +1,32 @@
-import React, { createContext, useContext, useState } from "react";
-import snoowrap from "snoowrap";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  SetStateAction,
+  Dispatch,
+} from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Linking from "expo-linking";
+
+import { CLIENT_ID, AUTH_URL } from "@env";
+
+interface Token {
+  access_token: string;
+  expires_in: string;
+}
 
 interface RedditContextValue {
   isLoggedIn: boolean;
+  redirectUri: string;
+  promptLogin: () => Promise<void>;
+  setToken: Dispatch<SetStateAction<Token | undefined>>;
 }
 
 const initialValue: RedditContextValue = {
   isLoggedIn: false,
+  redirectUri: "",
+  promptLogin: async () => {},
+  setToken: () => {},
 };
 
 export const RedditContext = createContext(initialValue);
@@ -16,12 +36,33 @@ export const useRedditContext = () => {
 };
 
 export const RedditProvider: React.FC = ({ children }) => {
+  const [reddit, setReddit] = useState();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [token, setToken] = useState<Token | undefined>();
+  const redirectUri = Linking.createURL("/auth/");
+
+  const promptLogin = async () => {
+    const params = new URLSearchParams();
+
+    params.append("client_id", CLIENT_ID);
+    params.append("response_type", "token");
+    //TODO: verify state before and after request
+    params.append("state", "state");
+    params.append("redirect_uri", redirectUri);
+    params.append("scope", "*");
+
+    const loginUrl = `${AUTH_URL}?${params.toString()}`;
+
+    await WebBrowser.openBrowserAsync(loginUrl);
+  };
 
   return (
     <RedditContext.Provider
       value={{
         isLoggedIn,
+        redirectUri,
+        promptLogin,
+        setToken,
       }}
     >
       {children}
