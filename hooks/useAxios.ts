0fdc21ab22aa38243a2174d366473ba169be1b72
Logan Kuzyk <logan@kuzyk.net>
2021-12-23 13:26:44 -0800

    Set axios base URL and update headers depending on if user is logged in

--- hooks/useAxios.ts
+++ hooks/useAxios.ts
@@ -5,7 +5,19 @@ import { useRedditContext } from "../contexts/RedditContext";
 
 export const useAxios = () => {
   const { current: instance } = useRef(axios.create());
-  const { isLoggedIn } = useRedditContext();
+  const { isLoggedIn, userCredentials, clientCredentials } = useRedditContext();
+
+  instance.defaults.baseURL = "https://oauth.reddit.com";
+
+  if (isLoggedIn && userCredentials) {
+    instance.defaults.headers.common[
+      "Authorization"
+    ] = `bearer ${userCredentials.accessToken}`;
+  } else if (clientCredentials) {
+    instance.defaults.headers.common[
+      "Authorization"
+    ] = `bearer ${clientCredentials.accessToken}`;
+  }
 
   return instance;
 };
