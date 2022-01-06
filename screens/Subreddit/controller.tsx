3b53fe3e07910e1a78d27cbfd17bbad1c7d35dc5
Logan Kuzyk <logan@kuzyk.net>
2022-01-06 11:59:36 -0800

    Initial submissions flatlist

--- screens/Subreddit/controller.tsx
+++ screens/Subreddit/controller.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ActivityIndicator } from "react-native-paper";
 
 import { SubredditScreenView } from "./view";
 import { RootStackParamList } from "../../types/navigation";
+import { useSubreddit } from "../../hooks/useSubreddit";
 
 interface SubredditScreenControllerProps {
   navigation: NativeStackNavigationProp<RootStackParamList, "Subreddit">;
@@ -10,5 +12,10 @@ interface SubredditScreenControllerProps {
 
 export const SubredditScreenController: React.FC<SubredditScreenControllerProps> =
   ({}) => {
-    return <SubredditScreenView />;
+    const submissions = useSubreddit("all");
+    if (submissions.isSuccess) {
+      return <SubredditScreenView submissions={submissions.data} />;
+    } else {
+      return <ActivityIndicator animating={submissions.isLoading} />;
+    }
   };
--- screens/Subreddit/view.tsx
+++ screens/Subreddit/view.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { FlatList, View } from "react-native";
 
-interface SubredditScreenViewProps {}
+import { renderItem } from "../../components/cards/Submission";
+import { Submission } from "../../types/reddit";
 
-export const SubredditScreenView: React.FC<SubredditScreenViewProps> = () => {
-  return <></>;
+interface SubredditScreenViewProps {
+  submissions: Submission[];
+}
+
+export const SubredditScreenView: React.FC<SubredditScreenViewProps> = ({
+  submissions,
+}) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginTop: 0,
+      }}
+    >
+      <FlatList
+        data={submissions}
+        keyExtractor={({ id }) => id}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 };
