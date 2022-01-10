63d37e3af64256523e7ad583339ce6a47b16fc8f
Logan Kuzyk <logan@kuzyk.net>
2022-01-10 11:24:36 -0800

    Initial custom search bar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { TextInput } from "react-native-paper";
+
+interface SearchBarProps {
+  placeholder: string;
+  onChangeText: (query: string) => void;
+  handleBlur: (e: any) => void;
+  value: string;
+  error: boolean;
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({
+  placeholder,
+  onChangeText,
+  handleBlur,
+  value,
+  error,
+}) => {
+  return (
+    <TextInput
+      placeholder={placeholder}
+      onChangeText={onChangeText}
+      onBlur={handleBlur}
+      value={value}
+      mode="outlined"
+      error={error}
+      autoCapitalize="none"
+    />
+  );
+};
