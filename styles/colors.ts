830f16ed48e11bb70023289eed90bbad391d98e5
Logan Kuzyk <logan@kuzyk.net>
2022-01-19 19:22:30 -0800

    Add 500 weight of all colors

--- styles/colors.ts
+++ styles/colors.ts
@@ -1,4 +1,13 @@
 export const colors = {
+  red: { "500": "#F44336" },
+  pink: { "500": "#E91E63" },
+  purple: { "500": "#9C27B0" },
+  deepPurple: { "500": "#673AB7" },
+  indigo: { "500": "#3F51B5" },
+  blue: { "500": "#2196F3" },
+  lightBlue: { "500": "#03A9F4" },
+  cyan: { "500": "#00BCD4" },
+  teal: { "500": "#009688" },
   green: {
     "50": "#E8F5E9",
     "100": "#C8E6C9",
@@ -11,6 +20,13 @@ export const colors = {
     "800": "#2E7D32",
     "900": "#1B5E20",
   },
+  lightGreen: { "500": "#8BC34A" },
+  lime: { "500": "#CDDC39" },
+  yellow: { "500": "#FFEB3B" },
+  amber: { "500": "#FFC107" },
+  orange: { "500": "#FF9800" },
+  deepOrange: { "500": "#FF5722" },
+  brown: { "500": "#795548" },
   grey: {
     "50": "#FAFAFA",
     "100": "#F5F5F5",
@@ -23,6 +39,7 @@ export const colors = {
     "800": "#424242",
     "900": "#212121",
   },
+  blueGrey: { "500": "#607D8B" },
 };
 
 export type ColorWeight =
