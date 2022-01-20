953322b1d7ff21c54696d12ed55201ce03880f59
Logan Kuzyk <logan@kuzyk.net>
2022-01-19 18:47:44 -0800

    Add initial useMoreChildren hook

---
+++ hooks/useMoreChildren.ts
@@ -0,0 +1,47 @@
+import { AxiosInstance } from "axios";
+import { useQuery } from "react-query";
+
+import { parseComments } from "../util/parseComments";
+import { useAxios } from "./useAxios";
+import {
+  RedditComment,
+  MoreChildren,
+  RawComment,
+  MoreChildrenResponse,
+} from "../types/reddit";
+
+const fetchMoreChildren = async (
+  axios: AxiosInstance,
+  moreChildren: MoreChildren,
+  linkFullname: string
+): Promise<
+  RedditComment | MoreChildren | Array<RedditComment | MoreChildren>
+> => {
+  const params = new URLSearchParams();
+  params.append("id", moreChildren.id);
+  params.append("children", moreChildren.children.join(","));
+  params.append("limit_children", "false");
+  params.append("api_type", "json");
+  params.append("link_id", linkFullname);
+  params.append("sort", "confidence");
+  const res = await axios.get<MoreChildrenResponse>(
+    `/api/morechildren?${params.toString()}`
+  );
+  const rawComments = res.data.json.data.things;
+  const comments = parseComments(rawComments);
+
+  return comments;
+};
+
+export const useMoreChildren = (
+  enabled: boolean,
+  moreChildren: MoreChildren,
+  linkFullname: string
+) => {
+  const axios = useAxios();
+  return useQuery(
+    ["moreChildren", moreChildren.id],
+    () => fetchMoreChildren(axios, moreChildren, linkFullname),
+    { enabled }
+  );
+};
