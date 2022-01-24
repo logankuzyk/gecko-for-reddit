0237cba143370dbc550fb15d066ef80a868b4bd8
Logan Kuzyk <logan@kuzyk.net>
2022-01-23 18:00:30 -0800

    Add upvote and downvote onPress handlers

--- components/reddit/comments/CommentActionMenu.tsx
+++ components/reddit/comments/CommentActionMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, useWindowDimensions } from "react-native";
 
 import { RedditComment } from "../../../types/reddit";
@@ -7,6 +7,7 @@ import { Upvote } from "../buttons/Upvote";
 import { Downvote } from "../buttons/Downvote";
 import { Save } from "../buttons/Save";
 import { Menu } from "../buttons/Menu";
+import { useVote } from "../../../hooks/useVote";
 
 interface CommentActionMenuProps {
   comment: RedditComment;
@@ -15,8 +16,34 @@ interface CommentActionMenuProps {
 export const CommentActionMenu: React.FC<CommentActionMenuProps> = ({
   comment,
 }) => {
+  const { mutate: castVote } = useVote(comment);
   const { width } = useWindowDimensions();
   const size = 18;
+  const [upvoteToggled, setUpvoteToggled] = useState<boolean>(false);
+  const [downvoteToggled, setDownvoteToggled] = useState<boolean>(false);
+  const [saveToggled, setSaveToggled] = useState<boolean>(false);
+
+  const handleUpvotePress = () => {
+    if (!upvoteToggled) {
+      castVote(1);
+      setUpvoteToggled(true);
+      setDownvoteToggled(false);
+    } else {
+      castVote(0);
+      setUpvoteToggled(false);
+    }
+  };
+
+  const handleDownvotePress = () => {
+    if (!downvoteToggled) {
+      castVote(-1);
+      setDownvoteToggled(true);
+      setUpvoteToggled(false);
+    } else {
+      castVote(0);
+      setDownvoteToggled(false);
+    }
+  };
 
   return (
     <View
@@ -33,8 +60,16 @@ export const CommentActionMenu: React.FC<CommentActionMenuProps> = ({
           justifyContent: "space-evenly",
         }}
       >
-        <Upvote onPress={() => {}} toggled={false} size={size} />
-        <Downvote onPress={() => {}} toggled={false} size={size} />
+        <Upvote
+          onPress={handleUpvotePress}
+          toggled={upvoteToggled}
+          size={size}
+        />
+        <Downvote
+          onPress={handleDownvotePress}
+          toggled={downvoteToggled}
+          size={size}
+        />
         <Save onPress={() => {}} toggled={false} size={size} />
         <Menu onPress={() => {}} toggled={false} size={size} />
       </View>
