92b6a4e3ce864b7d0baf01b765d746232c1c9d32
Logan Kuzyk <logan@kuzyk.net>
2022-01-30 17:27:24 -0800

    Go back after receiving auth token

--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -3,13 +3,17 @@ import React, { useEffect } from "react";
 import { RootDrawerScreenProps } from "../types/navigation";
 import { useRedditContext } from "../contexts/RedditContext";
 
-export default function AuthScreen({ route }: RootDrawerScreenProps<"Auth">) {
+export default function AuthScreen({
+  navigation,
+  route,
+}: RootDrawerScreenProps<"Auth">) {
   const { setAccessCode, setStateCode } = useRedditContext();
   useEffect(() => {
     const { code, state } = route.params;
     // reddit adds a "_#" to the end of the redirect!
     setAccessCode(code.slice(0, -2));
     setStateCode(state);
+    navigation.goBack();
   }, []);
 
   return <></>;
