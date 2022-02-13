f4ff22cc84340111972ca400455cee9bd16ec64f
Logan Kuzyk <logan@kuzyk.net>
2022-02-13 15:56:42 -0800

    Memoization

--- screens/Drawer/controller.tsx
+++ screens/Drawer/controller.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { DrawerNavigationHelpers } from "@react-navigation/drawer/lib/typescript/src/types";
 
 import { RootDrawerScreenView } from "./view";
@@ -15,22 +15,29 @@ export const RootDrawerScreenController: React.FC<
   const [query, setQuery] = useState<string>("");
 
   const subredditResults = useSearch(query);
-  const userResults: Array<ThingToLoad> | undefined = query
-    ? [
-        {
-          type: "user",
-          name: query,
-        },
-      ]
-    : undefined;
+  const userResults: Array<ThingToLoad> | undefined = useMemo(
+    () =>
+      query
+        ? [
+            {
+              type: "user",
+              name: query,
+            },
+          ]
+        : undefined,
+    [query]
+  );
 
-  const onChangeText = (input: string) => {
-    if (input.length > 2) {
-      setQuery(input);
-    } else {
-      setQuery("");
-    }
-  };
+  const onChangeText = useCallback(
+    (input: string) => {
+      if (input.length > 2) {
+        setQuery(input);
+      } else {
+        setQuery("");
+      }
+    },
+    [setQuery]
+  );
 
   return (
     <RootDrawerScreenView
